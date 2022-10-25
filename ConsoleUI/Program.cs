f35dc8d1ae6183a585295f23cc0878872be7b45a
Ahmet ALTINTAŞ <ahmtaltnts35@gmail.com>
2022-10-25 11:57:09 +0300

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,19 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using DataAccess.Concrete.InMemory;
+
+//SOLİD
+//Open Closed Program
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        ProductManager productManager = new ProductManager(new EfProductDal());
+
+        foreach (var product in productManager.GetByUnitPrice(50,60))
+        {
+            Console.WriteLine(product.ProductName);
+        }
+    }
+}
---
+++ Entities/Abstract/IEntity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Abstract
+{
+    // IEntity implement eden class bir veritabanı tablosudur
+
+    public interface IEntity
+    {
+
+    }
+}
